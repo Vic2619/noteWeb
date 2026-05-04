1da7124420f83b28a24ee5644ca3abcbac91abed
Vu2619 <huyvule2619@gmail.com>
2026-05-04 23:05:38 +0700

    Change Railway
    Updated database connection parameters for Railway.

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 declare(strict_types=1);
 
-$dbHost = 'db';      
-$dbName = 'noteweb';
+$dbHost = 'mysql.railway.internal';      
+$dbName = 'railway';
 $dbUser = 'root';
-$dbPass = 'root'; 
+$dbPass = 'OCJRnzgyKuzuNmRsXmsvRmKiSQzjhvBj'; 
 
 try {
     $pdo = new PDO(
